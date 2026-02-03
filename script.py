5ca14672276f00b3b5e8413ad71f8e81aaaa4b07
MuhamadMalik951 <muhamadmalik471@gmail.com>
2026-02-03 19:25:47 +0500

    Refactor SQL table creation logic and update connection string

--- script.py
+++ script.py
@@ -3,33 +3,23 @@ from sqlalchemy import create_engine, types
 
 
 file_path = r"C:\Users\david\Documents\job_work\Dropship\new_sql_practice\overstock invoice.xlsx"
-
 file = pd.read_excel(file_path, sheet_name=0, nrows=5)
-file_metadata = [c.strip().replace(" ", "_").lower() for c in pd.ExcelFile(file_path).sheet_names]
-print(file_metadata)
-print(file)
-columns = []
-table_name = file_metadata[0]
-column_names = {}
-for c in file.columns:
-    column_names[(c.strip().replace(" ", "_").lower())] = types.VARCHAR(255)
-
-print(column_names)
+table_name = pd.ExcelFile(file_path).sheet_names[0].strip().replace(" ", "_").replace("/", "_").replace(".", "")
 
 
-# column_types = {col: types.VarChar(255) }
+file.columns = [c.strip().replace(" ", "_").replace("/", "_").replace(".", "").lower() for c in file.columns]
+column_types = {c: types.VARCHAR(255) for c in file.columns}
 
-connection_string = "mysql+mysqlconnecter://root:malik@localhost/sql_practice?allow_local_infile=1"
+connection_string = "mysql+mysqlconnector://root:malik@localhost/sql_practice?allow_local_infile=1"
 # 
 engine = create_engine(connection_string)
-
 try:
     file.head(0).to_sql(
         name=table_name,
         con=engine,
         if_exists= 'replace',
         index=False,
-        dtype=column_names
+        dtype=column_types
     )
     print(f"\nSuccess! Table `{table_name}` has been created in MySQL.")
 
